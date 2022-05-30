6960d61e1c9a3134c6617233e4d4ee735f666d6a
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2022-05-30 11:28:52 +0100

    Add files via upload

---
+++ JAVA/script.js
@@ -0,0 +1,33 @@
+// single line comment
+
+console.log("switch");
+
+/*let a= 3;
+let b= 5;
+let c= a+b;*/
+
+let a = 4;
+let name= "udeme";
+name = "stephen";
+
+const b = 5 + 4;
+
+let oldprice = 30;
+let newprice = 40;
+const tax = 7.5;
+let price = tax * newprice;
+
+var goody = 45;
+
+alert("price");//this will display the sum of the tax multiplied by the new price.
+
+let newprice = 50; // this will have no value but an error bcos a variable with the name has already been created
+
+let newprices = 50; // this will run
+
+let boy = "Gospel"
+let BOY = "Gospel"
+
+let favour; // declared but undefined
+
+favour = "disturbing" // now defined
