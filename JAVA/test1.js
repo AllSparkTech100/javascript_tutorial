fd19da98a21184b6122e28fd718cef61ceee4ad3
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2022-06-01 12:17:03 +0100

    with test
    with test

---
+++ JAVA/test1.js
@@ -0,0 +1,11 @@
+let game = prompt ("what is your favourite game ?");
+confirm (`Is ${game} Your Best Game ?`);
+alert (`Thanks for choosing ${game}`);
+
+
+const Weapon = ["Gun", "Grenade", "Rhino Tank", "Sniper"];
+
+Weapon[1] = "game"
+console.log(Weapon);
+console.log(Weapon.length);
+
