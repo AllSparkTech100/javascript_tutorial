76b075dd271cf81ab63ae2fe2ce52f61e950322e
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2022-06-08 12:28:06 +0100

    Add files via upload
    mini calculator work How to Style with javascript

---
+++ JAVA STYLE/style.js
@@ -0,0 +1,84 @@
+// let spanEl = document.getElementById("good").textContent = "Hello"; // grabbing the html element
+// spanEl = document.getElementById("good");
+// //Or
+//  document.getElementById("good");
+
+// Styling with Javascript
+
+// let newColor = prompt("what is your best color ?");
+
+// let customerName = prompt("what is your name ?")
+
+
+
+// document.body.style.backgroundColor = "red";
+// document.getElementById("head").style.color = newColor;
+// document.getElementById("head").style.fontSize = "10rem";
+// document.getElementById("head").textContent = customerName;
+// document.getElementById("head").style.textTransform = "uppercase"
+// document.getElementById("solve").textContent+= sI
+
+//solving into html
+
+let P = 2500;
+let R = 10;
+let T = 2;
+let SI =( P * R  * T )/ 100;
+alert(SI)
+
+
+
+
+document.getElementById("container").style.marginLeft = "200px";
+
+document.getElementById("container").style.marginTop = "100px";
+
+document.getElementById("container").style.borderRadius = "10px";
+
+document.getElementById("container").style.borderRadius = "10px";
+
+document.getElementById("container").style.backgroundColor = "red";
+
+document.getElementById("container").style.width = "250px";
+
+document.getElementById("container").style.height = "300px";
+
+document.getElementById("calculator").style.marginLeft = "30px";
+
+document.getElementById("calculator").style.paddingTop = "0.5px";
+
+document.getElementById("calculator").style.textTransform = "uppercase";
+
+document.getElementById("calculator").style. fontSize = "14px";
+
+document.getElementById("calculator").style.color ="white";
+
+document.getElementById("calculator").style.fontFamily =
+  "cambria,sans-serif";
+
+  document.getElementById("inputs").style.marginLeft = "8px";
+
+  document.getElementById("inputs").style.fontFamily = "tahoma,sans";
+
+  document.getElementById("inputs").style.fontWeight = "bold";
+
+  document.getElementById("box").style.borderColor = "blue";
+  document.getElementById("bx").style.borderColor = "blue";
+  document.getElementById("bo").style.borderColor = "blue";
+
+  document.getElementById("box").style.border = "1px, solid";
+  document.getElementById("bx").style.border = "1px, solid";
+  document.getElementById("bo").style.border = "1px, solid";
+  
+  document.getElementById("box").style.borderRadius = "10px";
+
+  document.getElementById("bx").style.borderRadius = "10px";
+
+  document.getElementById("bo").style.borderRadius = "10px";
+
+  document.getElementById("button").style.backgroundColor = "blue";
+
+  document.getElementById("button").style.marginLeft = "85px";
+
+  document.getElementById("button").style.width = "30px";
+
