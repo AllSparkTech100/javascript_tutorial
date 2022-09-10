ef4b3da9a91e7727b52a43f1abcfd112d11c3984
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2022-09-10 02:59:55 +0100

    Add files via upload

---
+++ Image slider/Mode_Switch/Mode/mode.js
@@ -0,0 +1,8 @@
+const colorSwitch = document.getElementById('input-color-switch');
+colorSwitch.addEventListener('click', checkMode)
+
+function checkMode() {
+    console.log ("checking")
+}
+
+if (colorSwitch.)
