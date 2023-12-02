039fb620d21251d187d34a44d49b828cdf26e3cd
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2023-12-03 00:20:10 +0100

    Add files via upload

---
+++ JS Course/data_types.js
@@ -0,0 +1,106 @@
+// Data Types and Variables
+// (in computer science) Data is anything that is meaningful to the computer.
+/* Undefined = something that has not been defined.
+
+Null = simply say nothing.
+
+Boolean = a true or false statement.
+
+String =
+
+Symbol = an immutable value that is unique.
+
+Number = a number
+
+Object = can store a lot of key value types.
+*/
+
+
+//Declaring Variables
+/* using 
+
+var myName = this variable will be used throughout the whole program.
+
+let ourName = will be used within where it was declared.
+
+const pi = a variable that can never change.
+ */
+
+
+//Assigning Variable
+//e.g
+var a; Declaring
+var b = 2; Assigning using the assignment operator {=}
+
+//console.log allows you see things in the console.
+
+
+/* Addition of Numbers*/
+var sum = 12 + 12;
+console.log (sum);
+
+Subtraction
+var difference = 12 - 12;
+console.log (difference);
+
+Division
+var quotient = 12 / 12;
+console.log (quotient);
+
+Multiplication
+var product = 12 * 12;
+console.log (product);
+
+
+
+Incrementing
+var myVar = 87;
+myVar = myVar++;
+
+
+Decrementing
+var myVar = 87;
+myVar = myVar--;
+
+
+Decimal Numbers (Floating point nnumbers)
+var decimal = 5.7
+
+Looking for remainder
+
+var remainder = 11 % 3;
+remainder operator use to check if a number is odd or even.
+
+Augmented addition, multiplication, subtraction and division
+var a = 3;
+var b = 4;
+
+a +=23;
+b +=12;
+
+a *=2;
+b *=3;
+
+a -=1;
+b -=3;
+
+a /=2;
+b /=3;
+
+// String Variable
+
+var firstName = "Alan"
+var lastName = "Alaner"
+
+/* Code Output
+
+\' single quote
+\" double quote
+\\ backslash
+\n new-line
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+var myStr = "firstLine\n\t\\SecondLine\nThirdLine"
---
+++ JS Course/tutorial.js
@@ -0,0 +1,4 @@
+//commenting in javascript (comment are lines of code that javascript will ignore).
+
+// this is an in-line comment
+/* a multi-line comment */
